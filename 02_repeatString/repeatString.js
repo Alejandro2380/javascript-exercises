b1f841e18e286beef55d3bbcdfaced1f28306c98
Alejandro <mcgarvie.alejandro@gmail.com>
2021-12-24 14:12:02 -0800

    added code and edited test to pass program tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(para1, para2) {
+  const word = para1;
+  const num =  para2;
+  let string = "";
 
+  if (num < 0){
+    return "ERROR";
+  } else {
+      for (var i = 1; i <= num; i++){
+             string += word;
+      }
+
+    }
+    return string;
 };
 
 // Do not edit below this line
