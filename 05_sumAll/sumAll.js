9a364c43dd29d731ec9e773034ca38da32242271
Alejandro <mcgarvie.alejandro@gmail.com>
2021-12-25 11:24:48 -0800

    added code and edited tests to pass test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(sum1, sum2) {
+
+  let total = 0;
+  let number = sum1 + sum2;
+
+    if(typeof number === "number" && number > 0) {
+        if(sum1 < sum2){
+          for(var i = sum1; i <= sum2; i++){
+            total += i;
+            sum1++;
+          }
+        } else if (sum2 < sum1){
+            for(var i = sum2; i <= sum1; i++){
+              total += i;
+              sum2++;
+            }
+          }
+    return total;
+    } else {
+        return "ERROR";
+    }
+
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
